3405d8b6f46fc06e7cc959df891ca9c46d68ca70
GweeKentYee <kentyee2001@hotmail.my>
2021-04-22 11:38:30 +0800

    Panel

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -25,9 +25,7 @@ class ApiAuthController extends Controller
         $request['password']=Hash::make($request['password']);
         $request['remember_token'] = Str::random(10);
         $user = User::create($request->toArray());
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-        $response = ['token' => $token];
-        return response($response, 200);
+        return response($user, 200);
     }
 
     public function login (Request $request) {
