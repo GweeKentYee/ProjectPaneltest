e1d1effb61960d57e70822762620462054a111a0
GweeKentYee <kentyee2001@hotmail.my>
2021-04-22 11:41:52 +0800

    Panel

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -15,7 +15,7 @@ class ApiAuthController extends Controller
     //
     public function register (Request $request) {
         $validator = Validator::make($request->all(), [
-            'username' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
         if ($validator->fails())
