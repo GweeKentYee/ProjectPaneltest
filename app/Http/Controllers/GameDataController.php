7c8bf4e5370b15dfae3f7cea47349204dfc17b56
GweeKentYee <kentyee2001@hotmail.my>
2021-06-18 21:17:54 +0800

    Panel

--- app/Http/Controllers/GameDataController.php
+++ app/Http/Controllers/GameDataController.php
@@ -109,19 +109,21 @@ class GameDataController extends Controller
 
         if (in_array("players_id", $allcolumns)){
 
-            $exclude_columns = ['id', 'file','players_id', 'data_id', 'created_at', 'updated_at'];
+            $exclude_columns = ['file','players_id', 'data_id', 'created_at', 'updated_at'];
 
             $exclude_columns2 = ['id', 'file', 'players_id', 'data_id', 'created_at', 'updated_at'];
 
         } else {
 
-            $exclude_columns = ['id', 'file', 'data_id', 'created_at', 'updated_at'];
+            $exclude_columns = ['file', 'data_id', 'created_at', 'updated_at'];
 
             $exclude_columns2 = ['id', 'file', 'data_id', 'created_at', 'updated_at'];
 
         }
 
         $columns = array_diff($allcolumns, $exclude_columns); 
+
+        dd($columns);
 
         $special_columns = array_diff($allcolumns, $exclude_columns2);
     
